9ebe57c8f5745df039f4a793e12b10f0afe6aa86
Kingnusmi <kingnusmi14@gmail.com>
2022-09-27 15:00:53 -0400

    commit tree 6ecd44220c6d9ae711e7b6e8faafadfb2bc153b7

---
+++ index.js
@@ -0,0 +1,22 @@
+function shout(string){
+    return string.toUpperCase();
+}
+console.log(shout);
+function whisper(string){
+    return string.toLowerCase();
+}
+console.log(whisper);
+function logShout(string){
+    console.log(string.toUpperCase());
+}
+console.log(logShout);
+function logWhisper(string){
+    console.log(string.toLowerCase());
+}
+console.log(logWhisper);
+
+function sayHiToHeadPhonedRoomate(){
+ return shout(string);
+}
+
+    
